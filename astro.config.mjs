d78bc0bd4e0d5601847115713b2619ee096a7f23
Alexander Eremeev <alexander.eremeev@digistore24.team>
2022-10-14 20:57:25 +0200

    use gh-pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
+import vue from "@astrojs/vue";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "eremeev.de",
   // Enable Vue to support Vue components.
-  integrations: [vue(), tailwind()]
+  integrations: [vue(), tailwind()],
 });
