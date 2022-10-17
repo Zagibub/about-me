def5347175bb76f893038867419dd274648068b6
Alexander Eremeev <alexander.eremeev@digistore24.team>
2022-10-17 17:22:23 +0200

    login area

---
+++ src/components/Button.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+
+const props = defineProps({
+    label: { type: String, require: true },
+})
+</script>
+<template>
+    <button
+        class="h-10 rounded-lg transition duration-300 bg-palette-300 hover:bg-palette-200 hover:text-palette-400 hover:outline-offset-2 hover:outline-dashed  hover:outline-2  hover:outline-palette-200 focus:outline-offset-2 focus:outline-dashed  focus:outline-2  focus:outline-palette-200 focus:bg-palette-200 focus:text-palette-400">
+        {{props.label}}
+    </button>
+</template>
--- src/components/IconLink.vue
+++ src/components/IconLink.vue
@@ -6,6 +6,6 @@ const props = defineProps({
 })
 </script>
 <template>
-    <a class="flex justify-center items-center h-10 w-10 rounded-lg transition duration-300 bg-palette-300 hover:outline-offset-2 hover:outline-dashed  hover:outline-2  hover:outline-palette-200"
+    <a class="flex justify-center items-center h-10 w-10 rounded-lg transition duration-300 bg-palette-300 hover:bg-palette-200 hover:text-palette-400 hover:outline-offset-2 hover:outline-dashed  hover:outline-2  hover:outline-palette-200 focus:outline-offset-2 focus:outline-dashed  focus:outline-2  focus:outline-palette-200 focus:bg-palette-200 focus:text-palette-400"
         :href=props.link><i :class=props.icon></i></a>
 </template>
